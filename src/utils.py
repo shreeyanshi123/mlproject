6a04f45e2ac30a9ad78c7c3ed63ac80e18ecdb27
Shreeyanshi Gupta <158245215+shreeyanshi123@users.noreply.github.com>
2025-11-04 22:08:51 +0530

    hyper-paramter-tuning

--- src/utils.py
+++ src/utils.py
@@ -28,8 +28,16 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            params=param[list(models.keys())[i]]
 
+
+            gs=GridSearchCV(model,params,cv=3)
+            gs.fit(X_train,y_train)
+
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+            # model.fit(X_train,y_train)
 
 
             y_train_pred=model.predict(X_train)
